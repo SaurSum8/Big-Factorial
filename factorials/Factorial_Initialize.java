e649ec8fb589127eec70c61b9ead4a09eae8414c
SaurSum8 <78038123+SaurSum8@users.noreply.github.com>
2024-06-13 21:05:26 +0530

    Update Factorial_Initialize.java

--- factorials/Factorial_Initialize.java
+++ factorials/Factorial_Initialize.java
@@ -7,7 +7,7 @@ public class Factorial_Initialize {
 	public static void main(String[] args) {
 		
 		BigFactorial bf = new BigFactorial();
-		BigInteger value = bf.calculate(new BigInteger("1000000"), bf);
+		BigInteger value = bf.calculate(new BigInteger("1000000"));
 		
 		//Print the value (this might take some extra time, remove this statement if you are testing speed)
 		System.out.println(value);
